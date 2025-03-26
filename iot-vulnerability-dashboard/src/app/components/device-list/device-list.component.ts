994bba1f76091a86276822a58618a6a1a121429e
Gillian Masija <gillianmasija@gmail.com>
2025-03-26 01:20:07 +0000

    backend and front end

---
+++ iot-vulnerability-dashboard/src/app/components/device-list/device-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-device-list',
+  imports: [],
+  templateUrl: './device-list.component.html',
+  styleUrl: './device-list.component.css'
+})
+export class DeviceListComponent {
+
+}
